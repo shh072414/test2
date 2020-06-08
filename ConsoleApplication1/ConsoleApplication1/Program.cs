150c1f4bdd36fb711ad8dec4229d3ad122c989d3
shh072414 <48975628+shh072414@users.noreply.github.com>
2020-06-08 23:48:07 +0800

    2347

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bb = string.Empty;
+            string filepath = @"D:\test.ini";
+            string qq = string.Empty;
+            int aa = GetPrivateProfileSectionA("ccc",bb,1024,filepath);
+            //2347
+            Console.WriteLine(bb.ToString());
+            Console.Read();
+
+        }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int GetPrivateProfileSectionA(string lpAppName, string lpReturnedString, int nSize, string lpFileName);
+
+
+    }
+}
